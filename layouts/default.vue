7135cb1270202924da8a1684c69b173adfa3b064
Rafael Santana <rsantana@colum.edu>
2024-06-17 13:46:48 -0500

    A new file 'appFooter.vue' has also been added under the components/layout directory. It's a simple footer component displaying a text message in the center. An index page 'index.vue' has been included as well, with a placeholder component and a basic style for the body background color.

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  <slot/>
+  <LayoutAppFooter/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import Placeholder from "~/components/placeholder.vue";
+</script>
+
+<template>
+  <div>
+    <Placeholder/>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #1E1F26;
+}
+</style>
