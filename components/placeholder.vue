edab74a587afc9d16617979c78160ca51dd89341
Rafael Santana <rsantana@colum.edu>
2024-06-17 13:47:32 -0500

    Add placeholder.vue component for maintenance notice
    Implemented a new Vue component named 'placeholder.vue' that serves as a maintenance notice. This component displays a full-width, full-height box with a friendly message in the middle, informing customers that a quality website is being developed for them.

---
+++ components/placeholder.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-full h-screen flex justify-center items-center">
+    <div class="w-fit border border-amber-50 text-white p-10 text-2xl rounded-2xl space-y-2.5">
+      <div class="text-center text-3xl">
+        Pardon our dust!
+      </div>
+      <div>
+        Mobile Tires Pro customers, we are working on delivering a quality website for you
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
