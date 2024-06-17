ea29562704f961ce5c59a184ce42f943d1daf84f
Rafael Santana <rsantana@colum.edu>
2024-06-17 13:46:08 -0500

    Update app.vue to use NuxtLayout and SEO meta
    The HTML tag in app.vue has been replaced with the NuxtLayout component for better organization. Additionally, the script setup has been updated to include SEO meta related fields such as title, description, and social media information for better visibility on search engines and social platforms.

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <h1>Hello Mobile Tire Pro Customers!</h1>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
+
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Mobile Tire Pro',
+  ogTitle: 'Mobile Tire Pro',
+  description: 'Mobile Tire Pro website',
+  ogDescription: 'Mobile Tire Pro website',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
