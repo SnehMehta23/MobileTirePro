ab145b81552a43999a43399ee431054d7423a90c
Sneh Mehta <smehta@colum.edu>
2024-06-24 14:00:52 -0500

    feat: heroSection implemented, new icons/images added - vehicleType component made

---
+++ components/layout/heroSection.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import vehicleTypeSquare from "~/components/vehicleTypeSquare.vue"
+</script>
+
+<template>
+    <section class="flex justify-center gap-20 mt-8 text-left">
+        <div class="space-y-2 w-[500px] p-8">
+            <h1 class="text-5xl">Tire Service at Your Doorstep</h1>
+            <p class="text-2xl">Skip the tire shop hassle. We bring expert tire installation right to you!</p>
+            <p class="text-xl">Save time and stress with our mobile tire service. Whether you're at home or work we
+                don't
+                compromise on quality of service. Book your appointment by choosing your vehicle type or reach out for
+                any
+                questions. </p>
+            <div class="flex gap-2">
+                <vehicleTypeSquare vehicleType="Sedan" imageUrl="/images/sedan.png" />
+                <vehicleTypeSquare vehicleType="SUV" imageUrl="/images/SUV.png" />
+                <vehicleTypeSquare vehicleType="Truck" imageUrl="/images/pickuptruck.png" />
+                <vehicleTypeSquare vehicleType="EV" imageUrl="/images/electriccar.png" />
+            </div>
+            <hr class="border-t-4 border-gray-200">
+            <p class="text-lg font-medium text-orange-600 text-left mt-2 underline cursor-pointer">
+                Curious about your tires? Contact us for a no-cost assessment!
+            </p>
+        </div>
+
+        <div class="items-center flex">
+            <img class=""
+                src="/public/images/vecteezy_mobile-tire-service-abstract-concept-vector-illustration_35795024.eps.webp"
+                width="450" height="450" alt="">
+        </div>
+    </section>
+</template>
