b69dd1ecea95c2291a22e6835babba925ccb0121
Sneh Mehta <smehta@colum.edu>
2024-06-25 23:05:30 -0500

    feat: created markup for profile page

---
+++ pages/profile.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="flex justify-center gap-20 mt-8 ">
+        <!-- Menu Section Sidebar -->
+        <section>
+            <div>Vehicle Info</div>
+            <div>Billing Info</div>
+        </section>
+
+        <!-- Main Profile Widget -->
+        <section>
+            <div>
+                <!-- Profile Picture? Let's just do a simple OG twitter egg like profile pic for now -->
+                <div class="flex">
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                    <!-- Vehicle Tags -->
+                    <div>
+                        <span></span>
+                        <span></span>
+                    </div>
+
+                    <h3>Customer Name</h3>
+                    <!-- Can we make this be like a historical timeline type of vibe? -->
+                    <!-- History of Service -->
+                    <div>
+                        <p></p>
+                    </div>
+
+                    <!-- Tire Info Section -->
+                    <section>
+                        <div>
+                            <div>
+                                <h6></h6>
+                                <span></span>
+                            </div>
+                            <div>
+                                <h6></h6>
+                                <span></span>
+                            </div>
+                            <div>
+                                <h6></h6>
+                                <span></span>
+                            </div>
+                            <div>
+                                <h6></h6>
+                                <span></span>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
