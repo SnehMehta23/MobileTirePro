32c640f5ea585f72fcf091a8ed4a26fe443df97f
Rafael Santana <rsantana@colum.edu>
2024-06-27 12:36:30 -0500

    Refactor header navigation and add theme toggle
    This update refactors the navigation links in the header to be more dynamic and adds a theme toggle icon. Now, the navigation links are array mapped, not hardcoded, which makes the code more reusable and easier to maintain. Also, the added theme toggle function allows users to switch between dark and light themes, improving user experience and accessibility.

--- components/layout/appHeader.vue
+++ components/layout/appHeader.vue
@@ -6,13 +6,18 @@
           <img src="/public/images/logo_img2.png" alt="Mobile Tire Pro" class="h-36 w-auto min-w-[48px] mr-2">
         </div>
         <div class="hidden sm:flex items-center space-x-4">
-          <a href="#" class="text-gray-700 hover:text-gray-900">Home</a>
-          <a href="#" class="text-gray-700 hover:text-gray-900">About</a>
-          <a href="#" class="text-gray-700 hover:text-gray-900">Our Services</a>
-          <a href="#" class="text-gray-700 hover:text-gray-900">Log In</a>
+          <a v-for="link in links" :href="link.to" class="text-gray-700 hover:text-gray-900">{{ link.label }}</a>
           <button class="bg-red-500 text-white px-4 py-2 rounded-full hover:bg-red-600 transition duration-300">
             Book Now
           </button>
+          <div class="cursor-pointer">
+            <Icon
+                :name="iconName"
+                @click="toggleTheme"
+                :id="iconName"
+            />
+
+          </div>
         </div>
         <div class="sm:hidden flex items-center">
           <!-- Mobile menu button -->
@@ -30,10 +35,8 @@
     <!-- Mobile menu -->
     <div v-if="mobileMenuOpen" class="sm:hidden">
       <div class="px-2 pt-2 pb-3 space-y-1">
-        <a href="#" class="block px-3 py-2 text-gray-700 hover:text-gray-900">Home</a>
-        <a href="#" class="block px-3 py-2 text-gray-700 hover:text-gray-900">About</a>
-        <a href="#" class="block px-3 py-2 text-gray-700 hover:text-gray-900">Our Services</a>
-        <a href="#" class="block px-3 py-2 text-gray-700 hover:text-gray-900">Log In</a>
+        <a v-for="link in links" :href="link.to"
+           class="block px-3 py-2 text-gray-700 hover:text-gray-900">{{ link.label }}</a>
         <button
             class="w-full text-left bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600 transition duration-300">
           Book Now
@@ -55,7 +58,20 @@
 
 <script setup>
 
-const mobileMenuOpen = ref(false)
+const mobileMenuOpen = ref(false);
+const colorMode = useColorMode();
+const links = [
+  {to: '#', label: 'Home'},
+  {to: '#', label: 'About'},
+  {to: '#', label: 'Our Services'},
+  {to: '#', label: 'Log In'}]
 
+const iconName = computed(() =>
+    colorMode.preference === 'light' ? 'iconamoon:mode-dark-bold' : 'iconamoon:mode-light'
+);
+
+const toggleTheme = () => {
+  colorMode.preference = colorMode.preference === 'light' ? 'dark' : 'light';
+};
 
 </script>
