cf11e93807d655d187999b2b799713d6fcbd55e8
Rafael Santana <rsantana@colum.edu>
2024-06-28 12:45:46 -0500

    Update nuxt.config.ts formatting and yarn.lock
    The nuxt.config.ts file formatting was adjusted for readability and better structure. A theme toggle function was added, improving user experience and accessibility by allowing them to switch between dark and light themes. In addition, the dependencies in yarn.lock were updated. These changes make the code more dynamic, reusable and easier to maintain.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,13 @@ export default defineNuxtConfig({
         "@nuxtjs/google-fonts",
         "@nuxtjs/seo",
         "@vue-email/nuxt",
-      "nuxt3-leaflet",
+        "nuxt3-leaflet",
         "nuxt-mongoose",
         './modules/auth.module',
         '@nuxtjs/color-mode',
         "@nuxt/icon"
     ],
+    //@ts-ignore
     colorMode: {
         preference: 'light', // default value of $colorMode.preference
         fallback: 'light', // fallback value if not system preference found
@@ -20,24 +21,32 @@ export default defineNuxtConfig({
         componentName: 'ColorScheme',
         classPrefix: '',
         classSuffix: '-mode',
-        storageKey: 'nuxt-color-mode'
-    ],
+        storageKey: 'nuxt-color-mode',
+    },
     googleFonts: {
         families: {
-          Lato: [400, 700, 900] // Add the weights you need
-        },
+            Lato: [400, 700, 900] // Add the weights you need
+        }
+        ,
         display: 'swap' // This is optional but recommended for better performance
-    },
+    }
+    ,
     app: {
         head: {
             charset: 'utf-8',
-            viewport: 'width=device-width, initial-scale=1',
+            viewport:
+                'width=device-width, initial-scale=1',
         }
-    },
+    }
+    ,
     mongoose: {
         uri: process.env.MONGODB_URI,
-        options: {},
+        options:
+            {}
+        ,
         modelsDir: 'models',
-        devtools: true,
-    },
+        devtools:
+            true,
+    }
+
 })
