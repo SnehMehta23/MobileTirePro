86b00f575044700599a38883e93765225c272a08
Sneh Mehta <smehta@colum.edu>
2024-07-10 00:44:24 -0500

    feat: added real meta img and changed the title on ios and twitter links

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,13 +16,13 @@ export default defineNuxtConfig({
             // Open Graph / Facebook
             { property: "og:type", content: "website" },
             { property: "og:url", content: "https://mobiletirepro.net/" },
-            { property: "og:title", content: "Mobile Tire Pro" },
+            { property: "og:title", content: "Book Tire Service & Fitting | Mobile Tire Installation | Mobile Tire Pro" },
             { property: "og:description", content: "Mobile Tire Pro LLC: Expert mobile tire installation service for sedans, SUVs, trucks, and EVs. We come to you for convenient, on-site new tire fitting and TPMS service." },
             { property: "og:image", content: "/images/meta_img.webp" },
             // Twitter
             { name: "twitter:card", content: "summary_large_image" },
             { name: "twitter:url", content: "https://mobiletirepro.net/" },
-            { name: "twitter:title", content: "Mobile Tire Pro" },
+            { name: "twitter:title", content: "Mobile Tire Installation | Mobile Tire Pro" },
             { name: "twitter:description", content: "Mobile Tire Pro LLC: Expert mobile tire installation service for sedans, SUVs, trucks, and EVs. We come to you for convenient, on-site new tire fitting and TPMS service." },
             { name: "twitter:image", content: "/images/meta_img.webp" },
           ],
