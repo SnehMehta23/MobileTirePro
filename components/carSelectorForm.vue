adf62dd02bc036146db0bd6a9ad34b7d393a435b
Rafael Santana <rsantana@colum.edu>
2024-07-10 04:44:18 -0500

    Amend labels and add profile link in app layout
    Updated the labels for dropdown menus in the car modal from 'Year' to 'Make' and 'Model'. Also added a 'Profile' link to the app header that appears when the user is logged in, enhancing user navigation experience.

--- components/carSelectorForm.vue
+++ components/carSelectorForm.vue
@@ -1,40 +1,91 @@
 <!-- CarSelectorForm.vue -->
 <template>
-    <div
-        class="max-w-3xl p-6 bg-white dark:bg-gray-900 dark:text-white rounded-lg shadow-md dark:border-none border border-gray-200">
-        <h3 class="text-xl font-semibold dark:text-white text-dark-charcoal mb-4">Select your vehicle</h3>
-        <form @submit.prevent="handleSubmit" class="space-y-4">
-            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
-                <div v-for="(field, index) in ['year', 'make', 'model']" :key="index">
-                    <label :for="field"
-                        class="block text-left text-sm font-medium text-dark-charcoal dark:text-white mb-1 capitalize">{{
-                            field
-                        }}</label>
-                    <select :id="field" v-model="formData[field]"
-                        class="w-full p-2 text-sm border border-gray-300 rounded-md text-dark-charcoal dark:text-dark-charcoal focus:ring-vivid-red focus:border-vivid-red">
-                        <option value="">Select {{ field }}</option>
-                        <!-- Add options dynamically -->
-                    </select>
-                </div>
-            </div>
-            <div class="flex justify-start">
-                <button type="submit"
-                    class="px-6 py-2 text-sm bg-vivid-red text-white rounded-full hover:bg-crimson-red transition duration-300 shadow-lg focus:outline-none focus:ring-2 focus:ring-vivid-red focus:ring-offset-2">
-                    Check Quote
-                </button>
-            </div>
-        </form>
-    </div>
+  <div
+      class="max-w-3xl p-6 bg-white dark:bg-gray-900 dark:text-white rounded-lg shadow-md dark:border-none border border-gray-200">
+    <h3 class="text-xl font-semibold dark:text-white text-dark-charcoal mb-4">Select your vehicle</h3>
+    <form @submit.prevent="handleSubmit" class="space-y-4">
+      <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+
+
+        <div>
+          <label
+              class="block text-left text-sm font-medium text-dark-charcoal dark:text-white mb-1 capitalize">Year </label>
+          <select v-model="selectedYear"
+                  class="w-full p-2 text-sm border border-gray-300 rounded-md text-dark-charcoal dark:text-dark-charcoal focus:ring-vivid-red focus:border-vivid-red">
+            <option v-for="year in years" :value="year"> {{ year }}</option>
+            <!-- Add options dynamically -->
+          </select>
+        </div>
+        <div v-if="carData">
+          <label
+              class="block text-left text-sm font-medium text-dark-charcoal dark:text-white mb-1 capitalize">Make</label>
+          <select v-model="selectedMake"
+                  class="w-full p-2 text-sm border border-gray-300 rounded-md text-dark-charcoal dark:text-dark-charcoal focus:ring-vivid-red focus:border-vivid-red">
+            <option v-for="make in uniqueMakes" :value="make">{{ make }}</option>
+            <!-- Add options dynamically -->
+          </select>
+        </div>
+        <div v-if="selectedMake">
+          <label
+              class="block text-left text-sm font-medium text-dark-charcoal dark:text-white mb-1 capitalize">Model</label>
+          <select v-model="selectedModel"
+                  class="w-full p-2 text-sm border border-gray-300 rounded-md text-dark-charcoal dark:text-dark-charcoal focus:ring-vivid-red focus:border-vivid-red">
+            <option v-for="model in filteredModels" :value="model">{{ model }}</option>
+            <!-- Add options dynamically -->
+          </select>
+        </div>
+      </div>
+      <div class="flex justify-start">
+        <button type="submit"
+                class="px-6 py-2 text-sm bg-vivid-red text-white rounded-full hover:bg-crimson-red transition duration-300 shadow-lg focus:outline-none focus:ring-2 focus:ring-vivid-red focus:ring-offset-2">
+          Check Quote
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script setup>
 const formData = {
-    year: '',
-    make: '',
-    model: ''
+  year: '',
+  make: '',
+  model: ''
 }
 
+const startYear = 1992
+const endYear = 2026
+const years = Array.from({length: endYear - startYear + 1}, (v, i) => startYear + i)
+
+
+const selectedYear = ref('')
+const carData = ref('')
+const selectedMake = ref('')
+const selectedModel = ref('')
+const isLoading = ref(false)
+const emit = defineEmits(['refresh'])
+
+watch(selectedYear, async (newYear, oldYear) => {
+  if (newYear) {
+    const data = await $fetch(`/cars/${newYear}.json`)
+
+    carData.value = data;
+  }
+})
+
+const uniqueMakes = computed(() => {
+  const makes = carData.value.map(car => car.make)
+  return [...new Set(makes)]
+})
+
+const filteredModels = computed(() => {
+  if (!selectedMake.value) return []
+  return carData.value
+      .filter(car => car.make === selectedMake.value)
+      .map(car => car.model)
+})
+
+
 const handleSubmit = () => {
-    console.log('Selected:', formData)
+  console.log('Selected:', formData)
 }
 </script>
