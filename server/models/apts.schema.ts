380302536bee39e97208a66a020c0cd113a1b22c
Rafael Santana <rsantana@colum.edu>
2024-07-12 16:12:39 -0500

    Modify useLazyFetch calls in profile page
    Altered the useLazyFetch function calls in the profile page of the application. The updated version now prevents server-side requests, ensuring that these calls are client-side only, which might increase performance and reduce server load.

--- server/models/apts.schema.ts
+++ server/models/apts.schema.ts
@@ -22,7 +22,9 @@ export const aptsSchema = defineMongooseModel({
             enum: ['2 Tire installation (Large SUV/Truck/EV)',
                 '2 Tire installation (Sedan/Coupe/Small SUV)',
                 '4 Tire installation (Large SUV/Truck/EV)',
-                "4 Tire installation (Sedan/Coupe/Small SUV)"]
+                "4 Tire installation (Sedan/Coupe/Small SUV)",
+                "Seasonal Changeover (Tires Only)",
+                'Seasonal Changeover (Tire & Wheel Assemblies)']
         },
         //@ts-ignore
         tpm: {
