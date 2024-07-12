c6c64ff157cafe60d9077b298d45b0409ef94366
Rafael Santana <rsantana@colum.edu>
2024-07-11 21:19:49 -0500

    Add Square Web Payment SDK and integrate with Checkout flow
    Added Square Web Payment SDK to facilitate payment during the booking process. Two new server-side files were created, along with substantial changes to booking details on the client-side, notably in 'nuxt.config.ts' and 'bookings.vue'. In addition, the appointment booking process now shows a confirmation once booking and payment has been successfully completed.

---
+++ components/square/payment.vue
@@ -0,0 +1,183 @@
+<script setup lang="ts">
+onMounted(async () => {
+  loading.value = true;
+  await initializePaymentForm();
+  await initializeGooglePay()
+  loading.value = false;
+
+});
+
+const {price} = defineProps(['price'])
+
+const emit = defineEmits(['payment'])
+
+const appId = 'sandbox-sq0idb-2IrtLpJE_wYWee2XqXJx1A';
+const locationId = 'L5TYMTK1GRZGC';
+let card;
+let paymentStatus = ref("");
+let loading = ref(false);
+
+function buildPaymentRequest(payments) {
+  return payments.paymentRequest({
+    countryCode: 'US',
+    currencyCode: 'USD',
+    total: {
+      amount: price,
+      label: 'Total',
+    },
+  });
+}
+
+const initializeGooglePay = async () => {
+  if (!Square) {
+    throw new Error("Square.js failed to load properly");
+  }
+  const payments = Square.payments(appId, locationId);
+  try {
+    const paymentRequest = buildPaymentRequest(payments)
+    const googlePay = await payments.googlePay(paymentRequest);
+    await googlePay.attach('#google-pay-button', {
+      buttonSizeMode: 'fill',
+      buttonType: 'long'
+    });
+    const googlePayButton = document.getElementById('google-pay-button');
+    googlePayButton.onclick = async () => {
+      await handleGooglePaySubmission(googlePay)
+    }
+  } catch (e) {
+    console.error("Initializing Card failed", e);
+    return;
+  }
+}
+
+const initializePaymentForm = async () => {
+  if (!Square) {
+    throw new Error("Square.js failed to load properly");
+  }
+  const payments = Square.payments(appId, locationId);
+  try {
+    card = await payments.card();
+    await card.attach("#card-container");
+  } catch (e) {
+    console.error("Initializing Card failed", e);
+    return;
+  }
+};
+
+const tokenize = async (paymentMethod) => {
+  // console.log(paymentMethod)
+  const tokenResult = await paymentMethod.tokenize();
+  if (tokenResult.status === "OK") {
+    return tokenResult.token;
+  } else {
+    let errorMessage = `Tokenization failed with status: ${tokenResult.status}`;
+    if (tokenResult.errors) {
+      errorMessage += ` and errors: ${JSON.stringify(tokenResult.errors)}`;
+    }
+    throw new Error(errorMessage);
+  }
+}
+
+const handlePaymentMethodSubmission = async () => {
+  paymentStatus.value = "";
+  const token = await tokenize(card);
+  const {data, error} = await useFetch("/api/pay", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: {
+      locationId,
+      sourceId: token,
+    },
+  });
+  if (!error.value) {
+    paymentStatus.value = "Payment completed";
+    emit('payment')
+  } else {
+    paymentStatus.value = "Payment failed";
+  }
+};
+
+const handleGooglePaySubmission = async (googlePlay) => {
+  try {
+    paymentStatus.value = "";
+    const token = await tokenize(googlePlay);
+    const {data, error} = await useFetch("/api/pay", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: {
+        locationId,
+        sourceId: token,
+      },
+    });
+    if (!error.value) {
+      paymentStatus.value = "Payment completed";
+      emit('payment')
+
+    } else {
+      paymentStatus.value = "Payment failed";
+    }
+  } catch (e) {
+    console.error(e)
+  }
+};
+</script>
+
+<template>
+  <div>
+    <div class="w-full">
+      <form class="space-y-2.5" @submit.prevent="handlePaymentMethodSubmission">
+        <div v-if="loading">Loading...</div>
+        <div id="card-container"/>
+        <button>Pay ${{ price }}</button>
+        <div class="w-full" id="google-pay-button"></div>
+      </form>
+      <div v-if="paymentStatus" id="payment-status-container">
+        {{ paymentStatus }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+button {
+  color: #ffffff;
+  background-color: #006aff;
+  border-radius: 6px;
+  cursor: pointer;
+  border-style: none;
+  font-size: 16px;
+  line-height: 24px;
+  padding: 12px 16px;
+  width: 100%;
+}
+
+button:hover {
+  background-color: #005fe5;
+}
+
+button:active {
+  background-color: #0055cc;
+}
+
+button:disabled {
+  background-color: rgba(0, 0, 0, 0.05);
+  color: rgba(0, 0, 0, 0.3);
+}
+
+#payment-status-container {
+  width: fit-content;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #ffffff;
+  background: #1a1a1a;
+  display: flex;
+  padding: 12px;
+  border-radius: 6px;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1), 0px 0px 4px rgba(0, 0, 0, 0.1);
+  margin: auto;
+  margin-top: 36px;
+}
+</style>
