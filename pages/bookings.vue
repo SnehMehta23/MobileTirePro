b7962537f86b1a9d5f72a7f6b2f12c942fdcbff3
Rafael Santana <rsantana@colum.edu>
2024-07-18 11:51:45 -0500

    Update date and time handling in check.get.ts
    Moment-timezone is now used for handling date and time in check.get.ts to consider the local timezone 'America/Chicago'. Some lines of code were refactored to utilize timezone-aware date and time processing, potentially fixing issues related to displaying of incorrect dates and times due to differences in timezones.

--- pages/bookings.vue
+++ pages/bookings.vue
@@ -149,7 +149,7 @@ const computedPrice = computed(() => {
 
 <template>
   <div class="h-fit md:h-full  w-full flex justify-center items-center md:mt-20">
-    <div class="border-vivid-red bg-gray-900/10 md:border-2 rounded  md:w-1/3 px-4 py-4">
+    <div class="border-vivid-red bg-gray-900/10 md:border-2 rounded  md:w-2/3 lg:w-1/3 px-4 py-4">
       <div v-if="!showConfirmation" class=" flex justify-center items-center flex-col gap-3 w-full ">
         <div v-if="datesStatus === 'pending' "> Loading...</div>
         <div class="w-full p-2" v-else-if="datesStatus === 'success' && !selectedDate">
