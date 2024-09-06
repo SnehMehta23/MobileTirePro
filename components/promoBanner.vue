b26387b388a988339b590d02a15b7970dfee36c0
Rafael Santana <rsantana@colum.edu>
2024-09-06 18:26:10 -0500

    Commit message: Add spacing and duplicate LightDarkToggle in mobile menu
    The mobile menu button in appHeader.vue has been updated to include a duplicate LightDarkToggle and additional spacing between elements. The space-x-3 class has been added for a more cohesive and better-structured display on smaller screens.

--- components/promoBanner.vue
+++ components/promoBanner.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Promotional Banner -->
     <div class="w-full bg-vivid-red text-white text-center py-2 font-bold">
-        Book by August 31st and receive 25% off your installation
+        Book by September 30th and receive 25% off your installation
     </div>
 </template>
