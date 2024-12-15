efb1bdec3f8069edb13443dca1063da7359a49cb
Sneh Mehta <smehta@colum.edu>
2024-12-15 00:14:29 -0600

    feat: changed ahrefs script area

--- app.vue
+++ app.vue
@@ -8,4 +8,13 @@
 
 
 <script setup lang="ts">
+useHead({
+  script: [
+    {
+      src: 'https://analytics.ahrefs.com/analytics.js',
+      'data-key': 'Erhl4Y2wj+tly5AEGz/AQw',
+      defer: true
+    }
+  ]
+})
 </script>
