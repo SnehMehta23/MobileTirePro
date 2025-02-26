029377127fa837b3f1936b508f3b279f6d2828f8
Raph <raphy@raphsant.com>
2025-02-26 14:29:31 -0600

    Refactor service and appointment schemas, update API logic
    Updated service schema to include description and isActive fields, ensuring required properties are enforced. Enhanced appointment schema to handle tireCount and rimSize data. Adjusted relevant API endpoints to accommodate these changes and added type definitions for stronger typing.

--- server/api/services/add.post.ts
+++ server/api/services/add.post.ts
@@ -3,11 +3,11 @@ import {servicesSchema} from "~/server/models/services.schema";
 
 export default defineEventHandler(async (event) => {
     try {
-        const {name, rims, price, tireCount, time} = await readBody(event);
-        const newService = new servicesSchema({name, rims, price, tireCount, time})
+        const {name, price, tireCount} = await readBody(event);
+        const newService = new servicesSchema({name, price, tireCount,})
         await newService.save()
         return 'OK'
-    } catch (e) {
+    } catch (e:any) {
         return {
             status: '400',
             message: e.message,
